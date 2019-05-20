3ee3c81a9fd3891ec171545d6954a07b492db823
Maxwell Lasky <mlasky46@gmail.com>
2019-05-20 10:38:16 -0600

    attempt #420

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("*.jsgz", function(req, res, next) {
-  res.set("Content-Encoding", "gzip");
-  res.set("Content-Type", "text/javascript");
-  next();
-});
-app.get("*.datagz", function(req, res, next) {
-  res.set("Content-Encoding", "gzip");
-  next();
-});
+// app.get("*.jsgz", function(req, res, next) {
+//   res.set("Content-Encoding", "gzip");
+//   res.set("Content-Type", "text/javascript");
+//   next();
+// });
+// app.get("*.datagz", function(req, res, next) {
+//   //   res.set("Content-Encoding", "gzip");
+//   next();
+// });
 
-app.get("*.wasmgz", function(req, res, next) {
-  res.set("Content-Encoding", "gzip");
-  next();
-});
+// app.get("*.wasmgz", function(req, res, next) {
+//   res.set("Content-Encoding", "gzip");
+//   next();
+// });
 
-app.use(express.static("static"));
+app.use(express.static("static-uncompressed"));
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`App listening on port ${process.env.PORT || 3000}!`)
